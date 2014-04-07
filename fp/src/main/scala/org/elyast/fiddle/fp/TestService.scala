3b4c6c990bc85b0d6f415f8d708b1fdcffc4ae65
Lukasz Jastrzebski <lukasz.jastrzebski@gmail.com>
2014-04-06 22:51:26 -0700

    netty server is working

---
+++ fp/src/main/scala/org/elyast/fiddle/fp/TestService.scala
@@ -0,0 +1,15 @@
+package org.elyast.fiddle.fp
+
+import unfiltered.request._
+import unfiltered.response._
+
+import unfiltered.netty._
+
+object TestService extends async.Plan
+  with ServerErrorResponse {
+
+  def intent = {
+    case req @ GET(Path("/time")) => 
+      req.respond(ResponseString("Test"))
+  }
+}
