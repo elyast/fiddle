0ba355f7ae7b62b9acabbe5354d6d57cb1019d51
Lukasz Jastrzebski <lukasz.jastrzebski@gmail.com>
2014-04-15 11:41:06 -0700

    fixing setup

--- examples/examples.web/src/main/scala/org/elyast/fiddle/examples/web/Handler.scala
+++ examples/examples.web/src/main/scala/org/elyast/fiddle/examples/web/Handler.scala
@@ -5,6 +5,8 @@ class Handler {
   def start() {
     val port = System.getenv("PORT")
     println(s"hi ${port}")
+    val netty = unfiltered.netty.Http(port.toInt)
+    netty.handler(TestService)
     unfiltered.netty.Http(port.toInt).handler(TestService).run { s =>
         println(s)
       }
